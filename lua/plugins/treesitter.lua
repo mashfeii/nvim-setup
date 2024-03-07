8e02093d4fb41b33aea2871f26ec8035709b5441
mashfei-laptop <evil-corporaty@yandex.ru>
2024-03-08 00:38:51 +0300

    Format files

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,12 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      ensure_installed = { "c", "cpp", "lua", "vim", "javascript", "html", "css", "markdown", "markdown_inline", "latex" },
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
