13a0f29790ea8b9a47f505845dcc42f142cc4067
mashfei-laptop <evil-corporaty@yandex.ru>
2024-04-09 02:54:41 +0300

    Add something

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,9 +5,11 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set number")
 vim.g.mapleader = ";"
 
+--[[
 vim.keymap.set('i', '{', "{}<Esc>ha", {})
 vim.keymap.set('i', '(', "()<Esc>ha", {})
 vim.keymap.set('i', '[', "[]<Esc>ha", {})
 vim.keymap.set('i', '"', '""<Esc>ha', {})
 vim.keymap.set('i', "'", "''<Esc>ha", {})
 vim.keymap.set('i', '`', "``<Esc>ha", {})
+]]
