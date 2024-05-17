2b5dae60d44176a087481dfdb1d00f929d7e657f
mashfei-laptop <evil-corporaty@yandex.ru>
2024-05-17 07:26:15 +0300

    Add some parsers

--- lua/plugins/nord.lua
+++ lua/plugins/nord.lua
@@ -6,12 +6,12 @@ return {
     config = function()
       require("nord").setup({
         search = { theme = "vscode" },
-        styles = {
-          comments = { italic = false },
-          keywords = { italic = false, bold = true },
-          functions = { italic = false, bold = true },
-          variables = { italic = false },
-        },
+        -- styles = {
+        --   comments = { italic = false },
+        --   keywords = { italic = false, bold = true },
+        --   functions = { italic = false, bold = true },
+        --   variables = { italic = false },
+        -- },
       })
       -- vim.cmd([[colorscheme nord]])
     end,
--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -4,14 +4,14 @@ return {
 	priority = 1000,
 	opts = {
 		style = "storm",
-		styles = {
-			comments = { italic = false, bold = false },
-			keywords = {
-				italic = false,
-				bold = true,
-			},
-			functions = { italic = false, bold = true },
-		},
+		-- styles = {
+		-- 	comments = { italic = false, bold = false },
+		-- 	keywords = {
+		-- 		italic = false,
+		-- 		bold = true,
+		-- 	},
+		-- 	functions = { italic = false, bold = true },
+		-- },
 		lualine_bold = true,
 	},
 }
